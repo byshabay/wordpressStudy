e98cc7f35422c4aa6b6f4e0f4142f4e6983f0f78
Alyona Shabaykina <shabbaykina@mail.ru>
2021-09-02 17:04:30 +0300

    Add a front-page template

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Template part for displaying front page
+ *
+ * @link https://vk.com/feed
+ *
+ * @package ave
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
+    <h3> <?php the_content() ?> </h3>
+
+
+</article>
